3024a5821c8a27e65962751825a663ba3abc9cec
Vermaanug <Vermaanug1974@gmail.com>
2024-08-16 16:40:17 +0530

    Created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./Components/Header";
+
 const App =  () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">My App</h1>
+      <Header />
     </div>
   )
 }
