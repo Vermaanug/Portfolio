24fb5eab534b16a513ba0c52b62c6c0cdcdb9711
Vermaanug <Vermaanug1974@gmail.com>
2024-08-20 19:14:47 +0530

    Created Contact and Footer Component

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="relative z-10 bg-black flex justify-center custom-lg:-ml-28 2xl:-ml-72">
+      <div className="mx-2 flex flex-col gap-6 max-w-full lg:mb-10  ">
+        <div className="p-2">
+          <p className="font-nunito font-semibold uppercase  text-[#66FFCC] tracking-widest text-[11px] lg:text-sm ">
+            Have a project? or just looking to hire?
+          </p>
+          <h2 className="font-dmserif font-extrabold  text-white  text-4xl  md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl ">
+            Let's Work Together
+          </h2>
+          <p className="opacity-80 mt-3  text-white font-nunito md:w-[60%]  xl:w-[60%]">
+            Feel free to reach out if you're looking to hire, just want to
+            connect or see if we can build something amazing together.
+          </p>
+        </div>
+        <div className="mb-10">
+          <a
+            href="mailto:Vermaanug1974@gmail.com"
+            className="p-2 font-nunito font-semibold text-white hover:text-[#66FFCC] hover:underline underline-offset-4 decoration-2 hover:transition-all duration-300"
+          >
+            Get in touch →
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="relative z-10 bg-[#1C1C1C]">
+      <div className="flex p-4 justify-center items-center">
+        <p className="text-white font-roboto font-bold text-[12px] ">© 2024 Anurag Verma</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
